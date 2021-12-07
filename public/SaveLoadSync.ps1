61f53573da2706eb2f81fc1fd5300f4f8a6fb580
Kerbalnut <Kerbalnut@users.noreply.github.com>
2021-12-06 21:33:27 -0700

    Filling out parameters

--- public/SaveLoadSync.ps1
+++ public/SaveLoadSync.ps1
@@ -7,19 +7,38 @@ Function New-TaskTrackingInitiative {
 	#Requires -Version 3
 	<#
 	.SYNOPSIS
+	Creates the required files to start a MiniTaskMang project at folder path.
 	.DESCRIPTION
 	.NOTES
 	#>
-	[CmdletBinding()]
+	[CmdletBinding(DefaultParameterSetName = "PathName")]
 	Param(
-		[Parameter(Mandatory = $True, Position = 0,
-		           ValueFromPipeline = $True, 
-		           ValueFromPipelineByPropertyName = $True,
-		           HelpMessage = "Path to ...")]
+		[Parameter(Mandatory = $True, Position = 0, 
+				   ValueFromPipeline = $True, 
+				   ValueFromPipelineByPropertyName = $True, 
+				   HelpMessage = "Path to new project folder", 
+				   ParameterSetName = "PathName")]
 		[ValidateNotNullOrEmpty()]
-		[String]$Path
+		[Alias('Path')]
+		[String]$PathName,
+		
+		[Parameter(Mandatory = $True, Position = 0, 
+				   ValueFromPipelineByPropertyName = $True, 
+				   HelpMessage = "Path to source folder for project", 
+				   ParameterSetName = "SourcePath")]
+		[Alias('Source')]
+		[String]$SourcePath,
+		
+		[Parameter(Mandatory = $True, Position = 1, 
+				   ValueFromPipelineByPropertyName = $True, 
+				   HelpMessage = "Name for project", 
+				   ParameterSetName = "SourcePath")]
+		[Alias('Name')]
+		[String]$ProjectName
 		
 	)
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	
 	Return
 } # End of New-TaskTrackingInitiative function.
@@ -29,28 +48,40 @@ Set-Alias -Name 'New-ProjectInit' -Value 'New-TaskTrackingInitiative'
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-Function New-TaskTrackingInitiative {
-	[Alias('New-ProjectInit')]
+Function New-Task {
 	#Requires -Version 3
 	<#
 	.SYNOPSIS
+	Create a new action item.
 	.DESCRIPTION
 	.NOTES
 	#>
 	[CmdletBinding()]
 	Param(
-		[Parameter(Mandatory = $True, Position = 0,
-		           ValueFromPipeline = $True, 
-		           ValueFromPipelineByPropertyName = $True,
-		           HelpMessage = "Path to ...")]
+		[Parameter(Mandatory = $True, 
+				   Position = 0, 
+				   ValueFromPipeline = $True, 
+				   ValueFromPipelineByPropertyName = $True)]
 		[ValidateNotNullOrEmpty()]
-		[String]$Path
+		[Alias('Task','Name','n')]
+		[String]$TaskName,
+		
+		[Parameter(Mandatory = $True, 
+				   Position = 1, 
+				   ValueFromPipelineByPropertyName = $True)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Category','c')]
+		[String]$Context,
+		
+		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
+		[String]$ProjectPath
 		
 	)
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	
 	Return
-} # End of New-TaskTrackingInitiative function.
-Set-Alias -Name 'New-ProjectInit' -Value 'New-TaskTrackingInitiative'
+} # End of New-Task function.
 #-----------------------------------------------------------------------------------------------------------------------
 
 
